ac7683e3629c4078fc265dc52969652bab75442b
Pierre-David Bélanger <belp0028@uqar.ca>
2022-02-12 19:49:38 -0500

    F_RecursivePainting: Add logic + comment everything

--- RecursiveCallPainting/F_RecursivePainting.cs
+++ RecursiveCallPainting/F_RecursivePainting.cs
@@ -12,6 +12,8 @@ namespace RecursiveCallPainting
 {
     public partial class F_RecursivePainting : Form
     {
+        //2D array representing the grid
+        //When painting, values are stored there as we aren't working on the UI directly
         int[,] grid;
 
         public F_RecursivePainting()
@@ -21,7 +23,6 @@ namespace RecursiveCallPainting
 
         private void F_RecursivePainting_Load(object sender, EventArgs e)
         {
-
         }
 
         private void B_generateGrid_Click(object sender, EventArgs e)
@@ -31,18 +32,17 @@ namespace RecursiveCallPainting
 
         void CreateGrid(int width, int height)
         {
-            //Suspend the layout
-            SuspendLayout();
-
-            //Clear the grid first
+            /// 3 forward slashes for UI stuffs
+            //  2 forward slashes for logic stuffs
+            ///Clear the grid first
             flp_grid.Controls.Clear();
 
-            //Init flow layout panel size
-            //Width = Cell.Width * width , Height = Cell.height * height
+            ///Init flow layout panel size
+            ///Width = Cell.Width * width , Height = Cell.height * height
             flp_grid.Width  = 25 * width;
             flp_grid.Height = 25 * height;
 
-            //Resize form
+            ///Resize form
             this.Width =  20 + flp_grid.Width;
             this.Height = 85 + flp_grid.Height;
 
@@ -59,7 +59,7 @@ namespace RecursiveCallPainting
                     int value = rand.Next(0, 2);
                     //Assign to the 2d array (our grid)
                     grid[x, y] = value;
-                    // tb_cell_x 
+                    /// tb_cell_x 
                     TextBox cell = new TextBox
                     {
                         Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))),
@@ -71,60 +71,114 @@ namespace RecursiveCallPainting
                         TextAlign = System.Windows.Forms.HorizontalAlignment.Center,
                         Margin = new Padding(0)
                     };
-                    //Apply cell color
+                    ///Apply cell color
                     ApplyColor(cell);
 
                     //Add Cell OnClick event for painting
                     cell.Click += Cell_Click;
                     
-                    //Add cell to visual grid
+                    ///Add cell to visual grid
                     flp_grid.Controls.Add(cell);
                 }
             }
-
-            //Suspend the layout
-            ResumeLayout();
-            //Try force running garbage collection to prevent out of memoery issues
-            //Max size of 30x30 is fine tho and fits the screen so may not be required
-            GC.Collect();
         }
 
         void Cell_Click(object sender, EventArgs e)
         {
+            ///Current clicked cell
             TextBox cell = sender as TextBox;
-            DumbPaint(cell);
-            //SmartPaint(cell);
+            //Get x, y values of the cell (stored in its name : case_x_y) 
+            int x = Int32.Parse(cell.Name.Split('_')[1]);
+            int y = Int32.Parse(cell.Name.Split('_')[2]);
+            //Main painting method
+            PaintCells(x,y);
         }
 
-        //Without recursivity
-        void DumbPaint(TextBox cell)
+        //Get initial cell value 0 (white) or 1 (light gray)
+        int initialColor = 0;
+        //This method works on the main cell / the cell user clicked on
+        //Set initial array index to 2 (will be painted)
+        //Set direct neighbors indexes with same color than initial index (initialColor) to 2 (will be painted)
+        ///Apply array to the grid in UI
+        ///Paint cells considering their new values
+        void PaintCells(int x, int y)
         {
-            
+            //Initial color : the color we'll searching for in neighbors
+            initialColor = grid[x,y];
+
+            //Set initial index[x,y] (the index user clicked on) to 2 (will be painted) 
+            grid[x, y] = 2;
+
+            //Search neighbors in a clockwise pattern (up, right, down, left) 
+            //Up    -> grid[ x     , y - 1 ]
+            //Right -> grid[ x + 1 , y     ]
+            //Down  -> grid[ x     , y + 1 ]
+            //Left  -> grid[ x - 1 , y     ]
+            CheckNeighbor(x    , y - 1);
+            CheckNeighbor(x + 1, y    );
+            CheckNeighbor(x    , y + 1);
+            CheckNeighbor(x - 1, y    );
+
+            ///Regenerate grid
+            RegenerateGrid();
         }
 
-        //With recursivity
-        void SmartPaint(TextBox cell)
+        //Method used to check neighbors of the initial/main index/cell user clicked on
+        //Method then call itself to check neighbors of neighbors, etc
+        //Checks are added so method has limits => wont go out of range, does not process neighbors having a different color than the initalColor(user clicked cell initial color)
+        private void CheckNeighbor(int x, int y)
         {
+            //If index is out of range return
+            if (x == -1 || y == -1 || x >= grid.GetLength(0) || y >= grid.GetLength(1)) return;
 
+            //If not initial color or already set as "will be painted", skip this index
+            if (grid[x, y] != initialColor || grid[x, y] == 2) return;
+
+            //Else set value to 2 (will be painted)
+            grid[x, y] = 2;
+
+            //Up
+            CheckNeighbor(x, y - 1);
+            //Right
+            CheckNeighbor(x + 1, y);
+            //Down
+            CheckNeighbor(x, y + 1);
+            //Left
+            CheckNeighbor(x - 1, y);
+        }
+
+        #region UI 
+        ///Cycles through the UI grid, takes the matching grid array values and assign them to it (to the UI grid)
+        private void RegenerateGrid()
+        {
+            foreach (Control control in flp_grid.Controls)
+            {
+                TextBox cell = control as TextBox;
+                int cell_x = Int32.Parse(cell.Name.Split('_')[1]);
+                int cell_y = Int32.Parse(cell.Name.Split('_')[2]);
+                cell.Text = grid[cell_x, cell_y].ToString();
+                ApplyColor(cell);
+            }
         }
 
         void ApplyColor(TextBox cell)
         {
             switch (cell.Text)
             {
-                //White
+                ///White
                 case "0":
                     cell.BackColor = Color.White;
                     break;
-                //Light Gray
+                ///Light Gray
                 case "1":
                     cell.BackColor = Color.LightGray;
                     break;
-                //Dark Gray (Paint Color)
+                ///Dark Gray (Paint Color)
                 case "2":
                     cell.BackColor = Color.Gray;
                     break;
             }
         }
+        #endregion
     }
 }
